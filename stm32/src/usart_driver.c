d972e3dd4b7bc2917c1927da33fdef3a2cc104eb
Bogdan <101634538+bogdanstoicasn@users.noreply.github.com>
2024-10-13 19:50:12 +0300

    Update usart_driver.c

--- stm32/src/usart_driver.c
+++ stm32/src/usart_driver.c
@@ -1,11 +1,11 @@
 #include "../include/usart_driver.h"
 
-void usart1_transmit(uint8_t data)
+void usart1_transmit_byte(uint8_t data)
 {
   return;
 }
 
-void usart1_receive(void)
+void usart1_receive_byte(void)
 {
   return (uint8_t) 0;
 }
