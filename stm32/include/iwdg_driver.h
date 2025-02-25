fa48bbfe6e30e74df6326cd0eac4ea8dff400260
Bogdan <101634538+bogdanstoicasn@users.noreply.github.com>
2025-02-25 18:59:49 +0200

    iwdg done

--- stm32/include/iwdg_driver.h
+++ stm32/include/iwdg_driver.h
@@ -3,6 +3,22 @@
 
 #include "./util/stm32f103_def.h"
 
+#define IWDG_MAX_RELOAD 0x7FF
+#define IWDG_MIN_RELOAD 0x000
+
+/*
+ * Macros used for write/reset
+ */
+#define IWDG_KEY_UNLOCK 0x5555
+#define IWDG_KEY_RELOAD 0xAAAA
+#define IWDG_KEY_START 0xCCCC
+
+/*
+ * Checking when updating the prescale/reload value
+ * */
+#define IWDG_SR_PVU (1 << 0)
+#define IWDG_SR_RVU (1 << 1)
+
 typedef enum {
 	IWDG_PRESCALER_DIV_4 = 0,
 	IWDG_PRESCALER_DIV_8 = 1,
@@ -13,10 +29,39 @@ typedef enum {
 	IWDG_PRESCALER_DIV_256 = 6
 } iwdg_prescaler;
 
+/*
+ * @brief Enables Independent Watchdog with specified prescaler and reload values.
+ *
+ *  It starts the watchdog timer, which will reset the system if not refreshed
+ *  before timeout.
+ *
+ *
+ *  @param prescaler: Value used to divide the clock.
+ *  @param reload: Value used to set the timeout period.
+ *
+ */
 void iwdg_enable(iwdg_prescaler prescaler, uint16_t reload);
 
+/*
+ * @brief Changes the prescaler and reload values for the Independent Watchdog.
+ *
+ * Changes the prescale and reload while the IWDG is running.
+ *
+ *
+ * @param new_prescaler: New prescaler value for IWDG.
+ * @param new_reload: New reload value for IWDG.
+ *
+ */
+void iwdg_change_settings(iwdg_prescaler new_prescaler, uint16_t new_reload);
+
+/*
+ * @brief Resets the Independent Watchdog timer.
+ *
+ * Writes an appropiate  value to the Key Register so that the counter will be
+ * reloaded and start counting from it's initial values.
+ *
+ */
 void iwdg_reset();
 
-void iwdg_disable();
 
 #endif
--- stm32/src/iwdg_driver.c
+++ stm32/src/iwdg_driver.c
@@ -1,3 +1,42 @@
 #include "../include/iwdg_driver.h"
 
+void iwdg_enable(iwdg_prescaler prescaler, uint16_t reload)
+{
+	if(reload > IWDG_MAX_RELOAD)
+		reload = IWDG_MAX_RELOAD;
 
+	IWDG->kr = IWDG_KEY_UNLOCK;
+
+	IWDG->pr = prescaler;
+	while (IWDG->sr & IWDG_SR_PVU);
+
+	IWDG->rlr = reload;
+	while (IWDG->sr & IWDG_SR_RVU);
+
+	IWDG->kr = IWDG_KEY_START;
+
+	IWDG->kr = IWDG_KEY_RELOAD;
+}
+
+void iwdg_reset()
+{
+	IWDG->kr = IWDG_KEY_RELOAD;
+}
+
+void iwdg_change_settings(iwdg_prescaler new_prescaler, uint16_t new_reload)
+{
+	IWDG->kr = IWDG_KEY_RELOAD;
+
+	if (new_reload > IWDG_MAX_RELOAD)
+		new_reload = IWDG_MAX_RELOAD;
+
+	IWDG->kr = IWDG_KEY_UNLOCK;
+
+	IWDG->pr = new_prescaler;
+	while (IWDG->sr & IWDG_SR_PVU);
+
+	IWDG->rlr = new_reload;
+	while(IWDG->sr & IWDG_SR_RVU);
+
+	IWDG->kr = IWDG_KEY_RELOAD;
+}
