95d3c142d2432bfaff1c7ac508cda85a4fd6d24b
Bogdan <101634538+bogdanstoicasn@users.noreply.github.com>
2025-02-28 18:07:12 +0200

    mistakes corrected in wwdg

--- stm32/include/wwdg_driver.h
+++ stm32/include/wwdg_driver.h
@@ -3,11 +3,15 @@
 
 #include "./util/stm32f103_def.h"
 
+#define RCC_APB1ENR_WWDG_EN (1 << 11) /* Must enable wwdg clock source from RCC*/
+
 #define WWDG_CR_T_MASK (0x7F)
 #define WWDG_CR_WDGA (1 << 7) /* 0x40 */
 
+#define WWDG_CFR_W_MASK WWDG_CR_T_MASK
 #define WWDG_CFR_EWI (1 << 9)
 #define WWDG_CFR_EWIF (1 << 0)
+
 
 /* Right now unsafe */
 
--- stm32/src/wwdg_driver.c
+++ stm32/src/wwdg_driver.c
@@ -2,12 +2,20 @@
 
 void wwdg_init(uint8_t prescaler, uint8_t window, uint8_t counter)
 {
-	WWDG->cfr = ((prescaler & 0x03) << 7) | (window & WWDG_CR_T_MASK);
-	WWDG->cr = (counter & WWDG_CR_T_MASK) | WWDG_CR_WDGA; /* Enable WWDG */
+	/* clock source */
+	RCC->apb1enr |= RCC_APB1ENR_WWDG_EN;
+
+	/* window and prescaler */
+	WWDG->cfr = ((prescaler & 0x03) << 7) | (window & WWDG_CFR_W_MASK);
+
+	/* counter and wwdg enable */
+	WWDG->cr = (counter & WWDG_CR_T_MASK) | WWDG_CR_WDGA;
 }
 
 void wwdg_reset(uint8_t counter)
 {
+	/* counter always over 0x42 for safety */
+	counter = (counter < 0x42) ? 0x42 : counter;
 	WWDG->cr = (WWDG->cr & ~WWDG_CR_T_MASK) | (counter & WWDG_CR_T_MASK);
 }
 
