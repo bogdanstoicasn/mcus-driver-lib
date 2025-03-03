2e336b44c1a122da79ff0da7ae0794ceb6337e59
Bogdan <101634538+bogdanstoicasn@users.noreply.github.com>
2025-03-03 17:58:10 +0200

    328 make update and spi receive func

--- stm32/include/spi_driver.h
+++ stm32/include/spi_driver.h
@@ -3,4 +3,20 @@
 
 #include "./util/stm32f103_def.h"
 
+/* Master section */
+
+void spi_master_init(void);
+
+uint16_t spi_master_receive(void);
+
+void  spi_master_transmit(uint16_t data);
+
+
+/* Slave section */
+void spi_slave_init(void);
+
+uint16_t spi_slave_receive(void);
+
+void spi_slave_transmit(uint16_t data);
+
 #endif
--- stm32/src/spi_driver.c
+++ stm32/src/spi_driver.c
@@ -1,1 +1,18 @@
 #include "../include/spi_driver.h"
+
+uint16_t spi_master_receive(void)
+{
+	SPI1->dr = 0xFF;
+
+	/* Wait for RXNE */
+	while (!(SPI1->sr & (1 << 0)));
+
+	return (uint16_t)SPI1->dr;
+}
+
+uint16_t spi_slave_receive(void)
+{
+	while (!(SPI1->sr & (1 << 0))) ;
+	
+	return (uint16_t)SPI1->dr;
+}
