96f950f5bf0b0c5e50b45554868347a207c64a60
Bogdan <101634538+bogdanstoicasn@users.noreply.github.com>
2025-03-09 18:14:05 +0200

    base spi done

--- stm32/include/spi_driver.h
+++ stm32/include/spi_driver.h
@@ -3,20 +3,35 @@
 
 #include "./util/stm32f103_def.h"
 
+#define SPI_MASTER_CONFIG 0
+#define SPI_SLAVE_CONFIG  1
+
+#define SPI_DATA_8BIT  0
+#define SPI_DATA_16BIT 1
+
 typedef enum {
 	SPI_MODE_POLLING = 0,
 	SPI_MODE_INTERRUPT = 1
 } spi_mode;
 
+
+
 typedef struct {
 	volatile spi_def *SPIx;
-	uint8_t spi_num; /* used for easy debugging */
+	uint8_t spinum; /* used for easy debugging */
+	uint8_t priority;
+	uint8_t prescaler;
+	uint8_t cpol;
+	uint8_t cpha;
+	uint8_t dataform;
 	spi_mode mode;
+
 } spi_config;
 
-/* Master section */
+/* Init section */
+void spi_init(volatile spi_config *hspi);
 
-void spi_master_init(volatile spi_config *hspi);
+/* Master section */
 
 uint16_t spi_master_receive(volatile spi_config *hspi);
 
@@ -24,8 +39,6 @@ void  spi_master_transmit(volatile spi_config *hspi, uint16_t data);
 
 
 /* Slave section */
-
-void spi_slave_init(volatile spi_config *hspi);
 
 uint16_t spi_slave_receive(volatile spi_config *hspi);
 
--- stm32/src/spi_driver.c
+++ stm32/src/spi_driver.c
@@ -1,5 +1,38 @@
 #include "../include/spi_driver.h"
 
+void spi_init(volatile spi_config *hspi)
+{
+	/* Disable SPI */
+	hspi->SPIx->cr1 &= ~(1 << 6);
+
+	if (hspi->priority == SPI_MASTER_CONFIG)
+		hspi->SPIx->cr1 |= (1 << 2);
+	else
+		hspi->SPIx->cr1 &= ~(1 << 2);
+
+	/* Only in master mode */
+	hspi->SPIx->cr1 &= ~(0x07 << 3);
+	hspi->SPIx->cr1 |= (hspi->prescaler << 3);
+
+	if (hspi->cpol)
+		hspi->SPIx->cr1 |= (1 << 1);
+	else 
+		hspi->SPIx->cr1 &= ~(1 << 1);
+
+	if (hspi->cpha)
+		hspi->SPIx->cr1 |= (1 << 0);
+	else 
+		hspi->SPIx->cr1 &= ~(1 << 0);
+
+	if (hspi->dataform == SPI_DATA_16BIT)
+		hspi->SPIx->cr1 |= (1 << 11);
+	else 
+		hspi->SPIx->cr1 &= ~(1 << 11);
+
+	/* Enable SPI */
+	hspi->SPIx->cr1 |= (1 << 6);
+}
+
 void spi_master_transmit(volatile spi_config *hspi, uint16_t data)
 {
 	while (!(hspi->SPIx->sr &  (1 << 1)));
